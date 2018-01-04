4e5b3a636f4dc3f606e7ec1c02ef9ef3e5f27abd
Mihir Patel <themihirpatel@users.noreply.github.com>
2018-01-04 10:05:48 +0530

    Add files via upload

---
+++ quickstart.py
@@ -0,0 +1,25 @@
+from instapy import InstaPy
+
+insta_username = ''
+insta_password = ''
+
+# set headless_browser=True if you want to run InstaPy on a server
+try:
+    session = InstaPy(username=insta_username,
+                      password=insta_password,
+                      headless_browser=False)
+    session.login()
+
+    # settings
+    session.set_upper_follower_count(limit=2500)
+    session.set_do_comment(True, percentage=10)
+    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
+    session.set_dont_include(['friend1', 'friend2', 'friend3'])
+    session.set_dont_like(['pizza', 'girl'])
+
+    # actions
+    session.like_by_tags(['natgeo'], amount=1)
+
+finally:
+    # end the bot session
+    session.end()
